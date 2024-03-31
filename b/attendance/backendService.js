03e796be279f79c5e846fdd8dcb94abefbed995e
NAVEEN162002 <81414412+NAVEEN162002@users.noreply.github.com>
2024-03-31 17:16:18 +0530

    Delete b directory

--- b/attendance/backendService.js
+++
@@ -1,9 +0,0 @@
-// services/backendService.js
-
-import axios from 'axios';
-
-const backendUrl = 'https://sas-server-5e8c.onrender.com'; // Replace with your backend server URL
-
-export const fetchDataFromBackend = () => {
-  return axios.get(`${backendUrl}/api/data`);
-};
